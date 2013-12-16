3763bca06094fd6caa5f0d5b9d21dfcb23a75668
Лагодюк Юрий <cafebabe@yandex-team.com.ua>
2013-12-16 11:48:35 +0200

    Cucumber output html to target/cucumber

--- src/test/java/com/citi/datadelivery/cucumber/CucumberTestRunner.java
+++ src/test/java/com/citi/datadelivery/cucumber/CucumberTestRunner.java
@@ -6,6 +6,6 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = "pretty")
+@CucumberOptions(format = { "pretty", "html:target/cucumber" })
 public class CucumberTestRunner {
 }
